4bb10420d5c99848e4edf9fb87ae1dfa5e26bb52
Antonio Gasco <antoniofgasco@gmail.com>
2021-02-19 11:05:19 +0000

    adding-imgs

--- src/components/GameModal.jsx
+++ src/components/GameModal.jsx
@@ -5,13 +5,13 @@ import "./../styles/GameModal.css";
 class GameModal extends Component {
   state = {
     open: true,
-    data: {
-      title: "",
-      releaseDate: "",
-      platforms: [],
-      synopsis: "",
-      link: "",
-    },
+    // data: {
+    //   title: "",
+    //   releaseDate: "",
+    //   platforms: [],
+    //   synopsis: "",
+    //   link: "",
+    // },
   };
 
   componentWillMount() {
@@ -25,11 +25,26 @@ class GameModal extends Component {
   };
 
   render() {
+    console.log(this.props.data);
+    const { data } = this.props;
     return (
       <div id="gameModal" className="gameModal">
         <Modal isOpen={this.state.open}>
-          <p>All data of the game goes here</p>
-          <button onClick={this.props.closeModal}>Close</button>
+          <div className="gameModal__left">
+            <div className="gameModal__leftTop">
+              <img src={data.modalLogo} alt={data.title + "'s logo"} />
+            </div>
+            <div className="gameModal__leftBottom"></div>
+          </div>
+          <div className="gameModal__right">
+            <img src={data.modal_bg} alt={data.title + "'s logo"} />
+          </div>
+          <button
+            className="gameModal__closeBtn"
+            onClick={this.props.closeModal}
+          >
+            Close
+          </button>
         </Modal>
       </div>
     );
